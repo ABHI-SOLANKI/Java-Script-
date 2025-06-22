7fcd33415437fd08035dab6623940a5391787445
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-06-22 22:06:27 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 136464
+let accountEmail = "abhi@google.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+
+// accountId = 5
+
+accountEmail ="abh@ie"
+accountPassword = "4445"
+accountCity = "PBR"
+
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword ,accountCity])
