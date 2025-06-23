a013cd835481414a3a14c4abe5d5160a31dd2d42
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-06-23 22:15:50 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert( 3 + 3 ) we are using node js not browser
+
+console.log( 3 + 3) // code readability shoud be high
+console.log("abhi")
+
+let name = "abhi"
+let age = 20
+let isLoggedIn = true
+let steate;
+
+
+// number => 2 to power 53
+// bigint 
+// srting => "Write in Dolble code "" "
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => uniue
+
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
