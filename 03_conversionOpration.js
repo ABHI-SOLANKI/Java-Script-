b64c88849e28806fa9081f7fd2c50110ed5beb00
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-06-24 22:48:00 +0530

    Add files via upload

---
+++ 03_conversionOpration.js
@@ -0,0 +1,28 @@
+let score = 12
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "12" => 12
+// "12abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 45
+
+let srtingNumber = String(someNumber)
+console.log(srtingNumber);
+console.log(typeof srtingNumber);
