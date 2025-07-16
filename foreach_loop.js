54976d9d514eebc8327522c434a841f759503722
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-07-16 09:20:02 +0530

    Add files via upload

---
+++ foreach_loop.js
@@ -0,0 +1,39 @@
+const coding = ["java", "c", "py", "javascript", "react"];
+
+// coding.forEach(function (val) {
+//   console.log(val);
+// });
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printme (item) {
+//     console.log(item);
+// }
+// coding.forEach(printme)
+
+// coding.forEach((item, index, arr) => {
+//   console.log(item, index, arr);
+// });
+
+const mycoding = [
+  {
+    langname: "javascript",
+    langfilename: "js",
+  },
+  {
+    langname: "java",
+    langfilename: "java",
+  },
+  {
+    langname: "paython",
+    langfilename: "py",
+  },
+];
+
+mycoding.forEach((item) => {
+
+    console.log(item.langname);
+    
+});
