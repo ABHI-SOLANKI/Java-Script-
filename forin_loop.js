64590650ab1ec8bbe4df74134e64d71c7ace1695
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-07-17 09:21:44 +0530

    Add files via upload

---
+++ forin_loop.js
@@ -0,0 +1,17 @@
+const myobject = {
+    js:'JavaScript',
+    Cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple '
+}
+
+for (const key in myobject) {
+    // console.log(`${key} ShortCut is for ${myobject[key]}`);
+    
+}
+
+const programming = ["js","rb","py","java","cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
