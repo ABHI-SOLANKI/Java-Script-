b1b5418a8cff5e8ebc89c0a3b1546cb01b01939a
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-07-18 09:26:34 +0530

    Add files via upload

---
+++ forof_loop.js
@@ -0,0 +1,41 @@
+// for of 
+
+// ["" , "" , ""]
+// {"" , "" , ""}
+
+const arr = [1,2,3,4,5,]
+for (const num of arr) {
+    // console.log(num);    
+}
+
+const greetings = "Hello world!!!" 
+for (const greet of greetings) {
+    // console.log(`each char is ${greet}`);
+    
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN',"INDIA")
+map.set('USA',"United State Of Amrerica")
+map.set('Fr',"Feance")
+
+// console.log(map);
+
+// for (const [key , value] of map) {
+    // console.log(key, ':-' , value);
+    
+// }
+
+
+const myobject ={
+    'game1': 'NSF',
+    'game2': 'Pubg'
+}
+
+// for (const [ket,value] of myobject) {
+//         console.log(key, ':-' , value);
+// }
+
+
