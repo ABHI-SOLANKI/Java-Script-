1d440be5806804af5a726f0a71b750bc5b945409
Abhishek Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2025-07-21 09:31:55 +0530

    Add files via upload

---
+++ while_dowhile.js
@@ -0,0 +1,22 @@
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`${i}`);
+//     i = i + 2
+// }
+
+
+// let mtarray = [`abhi`,`ronak`,`jayesh`]
+
+// let arr = 0
+// while (arr < mtarray.length) {
+//     console.log(`value ${mtarray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+let scr = 11
+do {
+    console.log(`score is ${scr}`);
+    scr++
+} while (scr <= 10);
